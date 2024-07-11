3f7af5a90b90ddff9f68796ffa56bdf190bac289
Punith B M <punith@designstring.com>
2024-07-11 15:36:10 +0530

    feat: latest updates

--- src/commands/1inch.ts
+++ src/commands/1inch.ts
@@ -4,20 +4,20 @@ import { checkLock } from '@middlewares/checkLock'
 import { clarifyIfPrivateMessages } from '@helpers/clarifyIfPrivateMessages'
 
 export function setup1inchInfo(bot: Telegraf<Context>) {
-  bot.command(['1inch'], sendInfo)
+  bot.command(['surge'], sendInfo)
 }
 
 export function sendInfo(ctx: Context) {
   if (ctx.update.message?.date) {
     console.log(
-      'Replying to 1inch',
+      'Replying to Surge',
       Date.now() / 1000 - ctx.update.message?.date
     )
   }
 
-  const aboutOneInch = strings(ctx.dbchat, 'oneInchInfo');
+  const aboutOneInch = strings(ctx.dbchat, 'surgeInfo');
   const link =
-      '[1inch Network](http://1inch.io/?utm_source=shieldy_en&utm_medium=cpc&utm_campaign=powered) ([iOS](https://apps.apple.com/app/apple-store/id1546049391?pt=122481420&ct=shieldy_ru&mt=8))';
+      '[Surge Build](http://surge.build/?utm_source=shieldy_en)';
 
   return ctx.replyWithMarkdown(`${aboutOneInch}\n\n${link}`, {
     disable_web_page_preview: false,
